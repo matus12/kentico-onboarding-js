19dc8a0e78b425aed1e6f3ca8b3641ed0da870fb
Matus Backor <MatusB@kentico.com>
2017-10-19 12:13:59 +0200

    [KA-200] Refactor TsComponent as function

--- src/components/TsComponent.tsx
+++ src/components/TsComponent.tsx
@@ -1,16 +1,13 @@
 import * as React from 'react';
+import {StatelessComponent} from "react";
 
 interface ITsComponentProps {
   name: string;
 }
 
-export class TsComponent extends React.Component<ITsComponentProps, undefined> {
-  render() {
-    return (
-      <h3 className="text-danger" title="https://lingojam.com/FancyTextGenerator">
-        Ⓗⓔⓛⓛⓞ ⓕⓡⓞⓜ
-        <b>{this.props.name}</b> [̲̅T][̲̅y][̲̅p][̲̅e][̲̅S][̲̅c][̲̅r][̲̅i][̲̅p][̲̅t] ⓒⓞⓜⓟⓞⓝⓔⓝⓣ
-      </h3>
-    );
-  }
-}
+export const TsComponent: StatelessComponent<ITsComponentProps> = ({ name }) => (
+  <h3 className="text-danger" title="https://lingojam.com/FancyTextGenerator">
+    Ⓗⓔⓛⓛⓞ ⓕⓡⓞⓜ
+    <b>{name}</b> [̲̅T][̲̅y][̲̅p][̲̅e][̲̅S][̲̅c][̲̅r][̲̅i][̲̅p][̲̅t] ⓒⓞⓜⓟⓞⓝⓔⓝⓣ
+  </h3>
+);
