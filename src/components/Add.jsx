482394e1e5be01c22c3d94e7b47ed8b13151f55f
Matus Backor <MatusB@kentico.com>
2017-10-19 12:01:13 +0200

    Reformat parameters

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -4,15 +4,16 @@ import PropTypes from 'prop-types';
 export class Add extends PureComponent {
   constructor(props) {
     super(props);
+
     this.state = ({
       inputValue: '',
     });
   }
+
   handleOnChange = (event) => {
     this.setState({
       inputValue: event.target.value,
     });
-    this.props.handleOnChange(event.target.value);
   };
   handleAdd = () => {
     this.props.handleAdd(this.state.inputValue);
@@ -20,13 +21,24 @@ export class Add extends PureComponent {
       inputValue: '',
     });
   };
+
   render() {
     return (
       <li className="list-group-item">
         <div className="col-xs-4">
-          <input className="form-control" value={this.state.inputValue} onChange={this.handleOnChange} />
+          <input
+            className="form-control"
+            value={this.state.inputValue}
+            onChange={this.handleOnChange}
+          />
         </div>
-        <button type="button" className="btn btn-light" onClick={this.handleAdd}>Add</button>
+        <button
+          type="button"
+          className="btn btn-light"
+          onClick={this.handleAdd}
+        >
+          Add
+        </button>
       </li>
     );
   }
