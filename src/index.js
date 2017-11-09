fcfbad2275c37fd843b57473b2276de0b349158f
Matus Backor <MatusB@kentico.com>
2017-11-09 09:49:47 +0100

    KA-202 Move state to Redux store

--- src/index.js
+++ src/index.js
@@ -2,10 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+import logger from 'redux-logger';
+import { getInitialItems } from './utils/getItems';
+import {
+  applyMiddleware,
+  compose,
+  createStore,
+} from 'redux';
+import { Provider } from 'react-redux';
 
 import { App } from './App.jsx';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middleware = [logger];
+
+const store = createStore(app, getInitialItems(), composeEnhancers(
+  applyMiddleware(...middleware)
+));
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />,
+  </Provider>,
   document.getElementById('app-root')
 );
