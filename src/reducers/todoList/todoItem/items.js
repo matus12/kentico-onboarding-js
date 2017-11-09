204b36dddc49122515d1ac8580bf61b953d579ec
Matus Backor <MatusB@kentico.com>
2017-11-09 10:25:16 +0100

    KA-202 Implement reducers

--- src/reducers/todoList/todoItem/items.js
+++ src/reducers/todoList/todoItem/items.js
@@ -1,1 +1,31 @@
-export const items = (prevState, action) => {};
+import { OrderedMap } from 'immutable';
+import { ItemRecord } from '../../../utils/itemRecord';
+import {
+  TODO_LIST_ITEM_CREATE,
+  TODO_LIST_ITEM_DELETE,
+  TODO_LIST_ITEM_UPDATE,
+} from '../../../constants/actionTypes';
+
+export const items = (previousState = OrderedMap(), action) => {
+  switch (action.type) {
+    case TODO_LIST_ITEM_CREATE:
+      return previousState.set(
+        action.payload.id,
+        new ItemRecord({
+          id: action.payload.id,
+          text: action.payload.text,
+        }));
+
+    case TODO_LIST_ITEM_DELETE:
+      return previousState.delete(action.payload.id);
+
+    case TODO_LIST_ITEM_UPDATE:
+      return previousState.set(
+        action.payload.item.id,
+        action.payload.item
+      );
+
+    default:
+      return previousState;
+  }
+};
