d4ac649f0ec03f04cfe6c4dd04cbc64143311415
Matus Backor <MatusB@kentico.com>
2017-11-10 12:39:27 +0100

    KA-201 Use cammelCase in EmptyItem

--- src/models/ListItem.js
+++ src/models/ListItem.js
@@ -1,9 +1,9 @@
 import { Record } from 'immutable';
 
-const EmptyItem = {
+const emptyItem = {
   id: '00000000-0000-0000-0000-000000000000',
   text: '',
   isEdited: false,
 };
 
-export const ListItem = Record(EmptyItem);
+export const ListItem = Record(emptyItem);
