3ac8aa51a8fd28ba448a950d49fc4d3177854376
Matus Backor <MatusB@kentico.com>
2017-11-13 11:09:48 +0100

    KA-202 Refactor List component

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,73 +1,37 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { AddedItemRedux } from '../containers/todo-list/AddedItem';
 import { TsComponent } from './TsComponent.tsx';
-import { Item } from './Item';
-import { ListItem } from '../models/ListItem';
+import { ItemRedux } from '../containers/todo-list/Item';
+import PropTypes from 'prop-types';
+import { OrderedMap } from 'immutable';
 
-export class List extends PureComponent {
-
-  deleteItem = (id) => {
-    this.props.onDeleteItem(id);
-  };
-
-  saveItem = (id, savedText) => {
-    const item = new ListItem({
-      id,
-      text: savedText,
-      isEdited: false,
-    });
-
-    this.props.onUpdateItem(item);
-  };
-
-  setIsEdited = (id, isEdited) => {
-    const item = this.props.items.get(id);
-
-    const editedItem = new ListItem({
-      id,
-      text: item.text,
-      isEdited,
-    });
-
-    this.props.onUpdateItem(editedItem);
-  };
-
-  cancel = (id) => {
-    this.setIsEdited(id, false);
-  };
-
-  clickOnText = (id) => {
-    this.setIsEdited(id, true);
-  };
-
-  render() {
-    return (
+export function List(props) {
+  return (
+    <div className="row">
       <div className="row">
-        <div className="row">
-          <div className="col-sm-12 text-center">
-            <TsComponent name="𝕱𝖆𝖓𝖈𝖞" />
-          </div>
-        </div>
-        <div className="col-sm-12 col-md-offset-2 col-md-8">
-          <ul className="list-group">
-            {this.props.items
-              .entrySeq()
-              .map(([uniqueKey, item], index) =>
-                <Item
-                  key={uniqueKey}
-                  item={item}
-                  index={index + 1}
-                  onDeleteItem={this.deleteItem}
-                  onSaveItem={this.saveItem}
-                  onCancel={this.cancel}
-                  onTextClick={this.clickOnText}
-                />,
-              )
-            }
-            <AddedItemRedux />
-          </ul>
+        <div className="col-sm-12 text-center">
+          <TsComponent name="𝕱𝖆𝖓𝖈𝖞" />
         </div>
       </div>
-    );
-  }
+      <div className="col-sm-12 col-md-offset-2 col-md-8">
+        <ul className="list-group">
+          {props.items
+            .entrySeq()
+            .map(([uniqueKey, item], index) =>
+              <ItemRedux
+                key={uniqueKey}
+                item={item}
+                index={index + 1}
+              />,
+            )
+          }
+          <AddedItemRedux />
+        </ul>
+      </div>
+    </div>
+  );
 }
+
+List.propTypes = {
+  items: PropTypes.instanceOf(OrderedMap).isRequired,
+};
