87cb65746552cf93b590d1ab4b1acea8d935d38a
Matus Backor <MatusB@kentico.com>
2017-11-22 10:27:59 +0100

    KA-203 Type actions

--- src/actions/actionCreators.ts
+++ src/actions/actionCreators.ts
@@ -6,10 +6,11 @@ import {
 } from '../constants/actionTypes';
 import { generateId } from '../utils/generateId';
 import { insertItemFactory } from './insertItemFactory';
+import { IAction } from './IAction';
 
 export const insertItem = insertItemFactory(generateId);
 
-export const updateItem = (id, text) => ({
+export const updateItem = (id: string, text: string): IAction => ({
   type: TODO_LIST_ITEM_UPDATE,
   payload: {
     id,
@@ -17,21 +18,21 @@ export const updateItem = (id, text) => ({
   },
 });
 
-export const deleteItem = (id) => ({
+export const deleteItem = (id: string): IAction => ({
   type: TODO_LIST_ITEM_DELETE,
   payload: {
     id,
   },
 });
 
-export const editItem = (id) => ({
+export const editItem = (id: string) => ({
   type: TODO_LIST_ITEM_EDIT,
   payload: {
     id,
   },
 });
 
-export const cancelItemEditing = (id) => ({
+export const cancelItemEditing = (id: string) => ({
   type: TODO_LIST_ITEM_CANCEL_EDIT,
   payload: {
     id,
--- src/actions/insertItemFactory.ts
+++ src/actions/insertItemFactory.ts
@@ -1,7 +1,8 @@
 import { TODO_LIST_ITEM_INSERT } from '../constants/actionTypes';
+import { IAction } from './IAction';
 
-export const insertItemFactory = generateId =>
-  text => ({
+export const insertItemFactory = (generateId: () => string) =>
+  (text: string): IAction => ({
     type: TODO_LIST_ITEM_INSERT,
     payload: {
       id: generateId(),
