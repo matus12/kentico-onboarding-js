1b53b64097577828c5d623dd7b4a9ef8c9683410
Matus Backor <MatusB@kentico.com>
2017-12-06 15:07:35 +0100

    KA-203 Remove unnecessary properties when using with

--- src/reducers/todo-list/items/item.ts
+++ src/reducers/todo-list/items/item.ts
@@ -6,15 +6,12 @@ import {
 } from '../../../constants/actionTypes';
 import { ListItem } from '../../../models/ListItem';
 import { IAction } from '../../../actions/IAction';
-import { IListItem } from '../../../models/IListItem';
 
 export const item = (previousState: ListItem, action: IAction): ListItem => {
   switch (action.type) {
     case TODO_LIST_ITEM_EDIT:
     case TODO_LIST_ITEM_CANCEL_EDIT: {
-      const updatedItem: IListItem = {
-        id: previousState.id,
-        text: previousState.text,
+      const updatedItem = {
         isEdited: !previousState.isEdited,
       };
 
@@ -22,8 +19,7 @@ export const item = (previousState: ListItem, action: IAction): ListItem => {
     }
 
     case TODO_LIST_ITEM_UPDATE: {
-      const updatedItem: IListItem = {
-        id: previousState.id,
+      const updatedItem = {
         text: action.payload.text,
         isEdited: !previousState.isEdited,
       };
