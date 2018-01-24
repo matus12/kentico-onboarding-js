eef0d5fb5bda6c37d5dc7a316c4155d73f25c110
Matus Backor <MatusB@kentico.com>
2018-01-24 10:45:12 +0100

    KA-256 Refactor actionCreators tests

--- src/actions/actionCreators.test.ts
+++ src/actions/actionCreators.test.ts
@@ -1,157 +1,124 @@
-import thunk from 'redux-thunk';
-import configureMockStore from 'redux-mock-store';
-import {
-  ITEM_POST_ERROR,
-  ITEM_POST_SUCCESS,
-  ITEMS_FETCH_ERROR,
-  ITEMS_FETCH_SUCCESS, TODO_LIST_ITEM_INSERT
-} from '../constants/actionTypes';
 import { postItemFactory } from './postItemFactory';
 import { fetchItemsFactory } from './fetchItemsFactory';
-import { insertItem, setCallError, setCallSuccess } from './actionCreators';
+import { API_URL } from '../constants/apiUrl';
 
 const MockAdapter = require('axios-mock-adapter');
 const axios = require('axios');
-
-const url = 'v1/items';
-const middleware = [thunk.withExtraArgument(url)];
-const mockStore = configureMockStore(middleware);
-const store = mockStore({});
 const mock = new MockAdapter(axios);
 
-afterEach(() => {
-  store.clearActions();
-  // mock.restore();
-});
-
 describe('Async actions', () => {
+  const insertItem = jest.fn();
+  const setCallSuccess = jest.fn();
+  const setCallError = jest.fn();
+  const dispatch = jest.fn(input => input);
+
+  const fetchItems = fetchItemsFactory(
+    {
+      insertItem,
+      setCallSuccess,
+      setCallError,
+      url: API_URL,
+      axios
+    });
+  const postItem = postItemFactory(
+    {
+      insertItem,
+      setCallSuccess,
+      setCallError,
+      url: API_URL,
+      axios
+    });
 
   it('creates TODO_LIST_ITEM_INSERT, ITEMS_POST_SUCCESS on correct POST request', (done) => {
+    insertItem.mock.calls.length = 0;
+    setCallSuccess.mock.calls.length = 0;
     const postTestItem = {
       id: 'e1f5c5e4-7f5e-4aa0-9e52-117cc8267f12',
       text: 'item'
     };
-    const expectedActions = [
-      {
-        type: TODO_LIST_ITEM_INSERT,
-        payload: postTestItem
-      },
-      {
-        type: ITEM_POST_SUCCESS,
-        payload: {}
-      }
-    ];
-    mock.onPost(url).reply(201, {
+    mock.onPost(API_URL).reply(201, {
         Id: postTestItem.id,
         Text: postTestItem.text
       }
     );
-    const postItem = postItemFactory(
-      {
-        insertItem,
-        setCallSuccess,
-        setCallError
-      },
-      axios);
 
-    store.dispatch(postItem(postTestItem.text))
+    postItem(postTestItem.text)(dispatch)
       .then(() => {
-        expect(store.getActions()).toEqual(expectedActions);
+        expect(insertItem.mock.calls.length).toBe(1);
+        expect(setCallSuccess.mock.calls.length).toBe(1);
         done();
       })
       .catch(err => console.log(err));
   });
 
   it('creates TODO_LIST_ITEM_INSERT, ITEMS_FETCH_SUCCESS on correct GET request', (done) => {
+    insertItem.mock.calls.length = 0;
+    setCallSuccess.mock.calls.length = 0;
     const fetchedTestItem = {
       id: 'e1f5c5e4-7f5e-4aa0-9e52-117cc8267f13',
       text: 'item'
     };
-    const expectedActions = [
-      {
-        type: TODO_LIST_ITEM_INSERT,
-        payload: fetchedTestItem
-      },
-      {
-        type: ITEMS_FETCH_SUCCESS,
-        payload: {}
-      }
-    ];
-    mock.onGet(url).reply(200, [
+    mock.onGet(API_URL).reply(200, [
         {
           Id: fetchedTestItem.id,
           Text: fetchedTestItem.text
         }
       ]
     );
-    const fetchItems = fetchItemsFactory(
-      {
-        insertItem,
-        setCallSuccess,
-        setCallError
-      },
-      axios);
 
-    store.dispatch(fetchItems())
+    fetchItems()(dispatch)
       .then(() => {
-        expect(store.getActions()).toEqual(expectedActions);
+        expect(insertItem.mock.calls.length).toBe(1);
+        expect(setCallSuccess.mock.calls.length).toBe(1);
         done();
       })
-      .catch(err => console.log(err));
+      .catch((err: any) => console.log(err));
+  });
+
+  it('creates TODO_LIST_ITEM_INSERT with correct argument', (done) => {
+    insertItem.mock.calls.length = 0;
+    const fetchedTestItem = {
+      id: 'e1f5c5e4-7f5e-4aa0-9e52-117cc8267f13',
+      text: 'item'
+    };
+    mock.onGet(API_URL).reply(200, [
+        {
+          Id: fetchedTestItem.id,
+          Text: fetchedTestItem.text
+        }
+      ]
+    );
+
+    fetchItems()(dispatch)
+      .then(() => {
+        expect(insertItem.mock.calls[0][0]).toBe(fetchedTestItem.text);
+        done();
+      })
+      .catch((err: any) => console.log(err));
   });
 
   it('creates ITEMS_FETCH_ERROR on bad GET request', (done) => {
-    const expectedAction = [
-      {
-        type: ITEMS_FETCH_ERROR,
-        payload: {
-          errorText: '400 Bad Request'
-        }
-      }
-    ];
+    setCallError.mock.calls.length = 0;
     mock.restore();
-    mock.onGet(url).reply(400);
+    mock.onGet(API_URL).reply(400);
 
-    const fetchItems = fetchItemsFactory(
-      {
-        insertItem,
-        setCallSuccess,
-        setCallError
-      },
-      axios);
-
-    store.dispatch(fetchItems())
+    fetchItems()(dispatch)
       .then(() => {
-        expect(store.getActions()).toEqual(expectedAction);
+        expect(setCallError.mock.calls.length).toBe(1);
         done();
       })
-      .catch(err => console.log(err));
+      .catch((err: any) => console.log(err));
   });
 
   it('creates ITEMS_POST_ERROR on bad POST request', (done) => {
+    setCallError.mock.calls.length = 0;
     const newItemText = 'item';
-    const expectedAction = [
-      {
-        type: ITEM_POST_ERROR,
-        payload: {
-          errorText: '400 Bad Request'
-        }
-      }
-    ];
-    // mock.restore();
-    mock.onGet(url).reply(400);
+    mock.restore();
+    mock.onGet(API_URL).reply(400);
 
-    const postItem = postItemFactory(
-      {
-        insertItem,
-        setCallSuccess,
-        setCallError
-      },
-      axios);
-
-    store.dispatch(postItem(newItemText))
+    postItem(newItemText)(dispatch)
       .then(() => {
-        expect(store.getActions()).toEqual(expectedAction);
+        expect(setCallError.mock.calls.length).toBe(1);
         done();
       })
       .catch(err => console.log(err));
