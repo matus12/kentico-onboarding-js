3544914734d9664796243c41bf6bbc0ea7672374
Matus Backor <MatusB@kentico.com>
2018-04-13 14:32:55 +0200

    KA-256 Separate ITodoListState inferface

--- src/models/IAppState.ts
+++ src/models/IAppState.ts
@@ -4,10 +4,12 @@ import { Uuid } from '../utils/generateId';
 import { FetchStatus } from './FetchStatus';
 import { Error } from './Error';
 
+export interface ITodoListState {
+  readonly items: Collection<Uuid, ListItem>;
+}
+
 export interface IAppState {
-  readonly todoList: {
-    readonly items: Collection<Uuid, ListItem>;
-  };
+  readonly todoList: ITodoListState;
   readonly error: Collection<Uuid, Error>;
   readonly fetchStatus: FetchStatus;
 }
--- src/reducers/todo-list/todoList.ts
+++ src/reducers/todo-list/todoList.ts
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import { items } from './items/items';
-import { IAppState } from '../../models/IAppState';
+import { ITodoListState } from '../../models/IAppState';
 
-export const todoList = combineReducers<IAppState['todoList']>({
+export const todoList = combineReducers<ITodoListState>({
   items,
 });
